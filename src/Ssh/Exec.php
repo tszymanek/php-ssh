cf87a58826b704fb056708b62adbc2fb987e5c08
Simon Podlipsky <simon@podlipsky.net>
2017-12-15 00:46:47 +0100

    Exec checking for return code optional

--- src/Ssh/Exec.php
+++ src/Ssh/Exec.php
@@ -13,25 +13,49 @@ use RuntimeException;
  */
 class Exec extends Subsystem
 {
+    private $error;
+
     protected function createResource()
     {
         $this->resource = $this->getSessionResource();
     }
 
-    public function run($cmd, $pty = null, array $env = array(), $width = 80, $height = 25, $width_height_type = SSH2_TERM_UNIT_CHARS)
-    {
-        $cmd .= ';echo -ne "[return_code:$?]"';
-        $stdout = ssh2_exec($this->getResource(), $cmd, $pty, $env, $width, $height, $width_height_type);
+    public function run(
+        $cmd,
+        bool $checkReturnCode = true,
+        $pty = null,
+        array $env = [],
+        $width = 80,
+        $height = 25,
+        $widthHeightType = SSH2_TERM_UNIT_CHARS
+    ) {
+        if ($checkReturnCode) {
+            $cmd .= ';echo -ne "[return_code:$?]"';
+        }
+
+        $stdout = ssh2_exec($this->getResource(), $cmd, $pty, $env, $width, $height, $widthHeightType);
         $stderr = ssh2_fetch_stream($stdout, SSH2_STREAM_STDERR);
+        $error = stream_get_contents($stderr);
+        if ($error !== '') {
+            $this->error = $error;
+        }
         stream_set_blocking($stderr, true);
         stream_set_blocking($stdout, true);
 
         $output = stream_get_contents($stdout);
+        if ($checkReturnCode === false) {
+            return $output;
+        }
         preg_match('/\[return_code:(.*?)\]/', $output, $match);
         if ((int) $match[1] !== 0) {
             throw new RuntimeException(stream_get_contents($stderr), (int) $match[1]);
         }
 
         return preg_replace('/\[return_code:(.*?)\]/', '', $output);
+    }
+
+    public function getError() : string
+    {
+        return $this->error;
     }
 }
